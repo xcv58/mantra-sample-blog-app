62795fec1d6edb958236bb682b469698ff20c34d
xcv58 <chenyihonglove@gmail.com>
2016-02-05 14:18:43 -0500

    isNaN code

--- client/modules/core/components/navigation.jsx
+++ client/modules/core/components/navigation.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import RaisedButton from 'material-ui/lib/flat-button';
+injectTapEventPlugin();
 
 const Navigation = () => (
   <div>
     <b> Navigation: </b>
     <a href="/">Home</a> |
     <a href="/new-post">New Post</a>
+    <RaisedButton label="Hello" />
   </div>
 );
 
